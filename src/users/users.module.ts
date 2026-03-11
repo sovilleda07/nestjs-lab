63905474ac69c9a4736325d555a72dbcf543fc2d
sovilleda07 <sovilleda07@gmail.com>
2026-03-11 17:53:33 -0600

    Middleware

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,32 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  Req,
+  RequestMethod,
+} from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { LoggerMiddleware } from './logger/logger.middleware';
+import { AuthMiddleware } from './auth/auth.middleware';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
 })
-export class UsersModule {}
+export class UsersModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    // Para todas las rutas del módulo
+    // consumer.apply(LoggerMiddleware).forRoutes('users');
+
+    // Para ciertas rutas y métodos
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes({
+        path: '/users',
+        method: RequestMethod.GET,
+      })
+      .apply(AuthMiddleware)
+      .forRoutes('users');
+  }
+}
